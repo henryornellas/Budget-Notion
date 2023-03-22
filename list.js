ac61a0ce9004cbc301293edb5c94c03c07329f4d
Henry <henrygabriellemos@hotmail.com>
2023-03-22 19:58:39 -0300

    Add DataBase

--- list.js
+++ list.js
@@ -1,45 +1,84 @@
-//This server passes data from webpage to server and back with EJS, and own EJS module.
+//This server passes data from webpage to server and back with EJS.
 
 //Requesting modules
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js'); //<<--- Requiring own EJS module.
+const mongoose = require('mongoose');
 
 const app = express();
 
 
-//EJS, Body Parser and Express modules setup
+//EJS, Body Parser, Express and mongoose modules setup
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+mongoose.connect('mongodb://127.0.0.1:27017/notionDB', {useNewUrlParser: true});
 
 
-//Set initial empty list arrays
-const items = ['Take cat for a walk', 'Buy blinker fluid'];
-const workItems = [];
+//Set new schema for DB
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model('item', itemsSchema);
 
 
-//Render local ejs page passing day and items array as parameters.
+//Default starter items
+const item1 = new Item ({
+  name: 'Buy blinker fluid'
+});
+const item2 = new Item ({
+  name: 'Take fish for a walk'
+});
+
+const defaultItems = [item1, item2];
+
+
+//Checks if items collection is empty to add starter items
 app.get('/', function(req, res){
 
-  const day = date.getDate();
+  Item.find({}).then(function(foundItems){
 
-  res.render('list', {listTitle: day, newListItems: items});
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems).then(function(){
+        console.log('Successfully saved');
+      }).catch(function(err){
+        console.log(err);
+      });
+      res.redirect('/');
+    }else{
+      res.render('list', {listTitle: 'Today', newListItems: foundItems});
+    }
+  });
 
 });
 
 
-//Submit button action adds new item to 'work' or normal list.
+//Submit button action adds new item to collection
 app.post('/', function(req, res){
 
-  const item = req.body.newTask
+  const taskName = req.body.newTask
 
-  if(req.body.list === 'Work'){
-    workItems.push(item);
-    res.redirect('/work');
-  }else{
-    items.push(item);
+  const item = new Item ({
+    name: taskName
+  });
+  item.save();
+
+  res.redirect('/');
+});
+
+
+
+app.post('/delete', async function(req, res){
+
+  const checkedItem = req.body.checked;
+
+  try {
+    const deleted = await Item.findByIdAndRemove(checkedItem);
+    console.log('DELETADO!');
     res.redirect('/');
+  }catch(err){
+    console.log(err);
   }
 
 });
